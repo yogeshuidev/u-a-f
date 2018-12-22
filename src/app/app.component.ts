d44e386c764e0b3c27135cdc866fbf72dc1d2346
Yogesh <uidev.yogesh@gmail.com>
2018-12-22 15:33:16 -0500

    Template fundamentals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'U-A-F';
+
+  //interpolation
+  hours : number = 160;
+  pay : number = 35;
+  credited :boolean = true;
+
+  //property binding
+  image : string ='assets/logo.svg'
+
+  //event binding
+  name : string ="Yogesh";
+  changeName() {
+    this.name = 'Yogi';
+  }
+  handleInput(event :any) {
+    this.name = event.target.value;
+  }
+
+  //local reference
+  getValue : string;
+  getName(value) {
+    this.getValue = value;
+    console.log(value); 
+ }
 }
