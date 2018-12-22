51e7eb910d79677fcd7180442dbad89d50d6ecd3
Yogesh <uidev.yogesh@gmail.com>
2018-12-22 16:51:18 -0500

    Rendering flows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age:number
+}
+interface Passenger {
+  id : number,
+  fullname: string,
+  checkedIn : boolean,
+  checkInDate : number | null,
+  children: Child[] | null
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'U-A-F';
@@ -31,4 +43,37 @@ export class AppComponent {
     this.getValue = value;
     console.log(value); 
  }
+
+ //*ngFor
+ passengers: Passenger[] = [{
+  id: 1,
+  fullname: 'Stephen',
+  checkedIn: true,
+  checkInDate: 1490742000000,
+  children: null
+}, {
+  id: 2,
+  fullname: 'Rose',
+  checkedIn: false,
+  checkInDate: null,
+  children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
+}, {
+  id: 3,
+  fullname: 'James',
+  checkedIn: true,
+  checkInDate: 1491606000000,
+  children: null
+}, {
+  id: 4,
+  fullname: 'Louise',
+  checkedIn: true,
+  checkInDate: 1488412800000,
+  children: [{ name: 'Jessica', age: 1 }]
+}, {
+  id: 5,
+  fullname: 'Tina',
+  checkedIn: false,
+  checkInDate: null,
+  children: null
+}];
 }
