d81c02e2575a0b2972a5ca3f0245f84a141988fe
Yogesh <uidev.yogesh@gmail.com>
2018-12-22 21:38:59 -0500

    move passenger data to ngOnInit method

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,41 +7,43 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['./passenger-dashboard.component.scss']
 })
 export class PassengerDashboardComponent implements OnInit {
-  
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkInDate: 1490742000000,
-    children: null
-  }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false,
-    checkInDate: null,
-    children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-  }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true,
-    checkInDate: 1491606000000,
-    children: null
-  }, {
-    id: 4,
-    fullname: 'Louise',
-    checkedIn: true,
-    checkInDate: 1488412800000,
-    children: [{ name: 'Jessica', age: 1 }]
-  }, {
-    id: 5,
-    fullname: 'Tina',
-    checkedIn: false,
-    checkInDate: null,
-    children: null
-  }];
+
+  passengers: Passenger[];
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    this.passengers = [{
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null
+    }, {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+    }, {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+      checkInDate: 1491606000000,
+      children: null
+    }, {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }]
+    }, {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+      checkInDate: null,
+      children: null
+    }];
   }
 
 }
