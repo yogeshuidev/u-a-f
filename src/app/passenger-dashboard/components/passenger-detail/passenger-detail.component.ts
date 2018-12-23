f5c34dffce860df64733924d8b3854d78aec63a7
Yogesh <uidev.yogesh@gmail.com>
2018-12-22 23:27:37 -0500

    Dynamic @Input values with *ngFor

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'app-passenger-detail',
   templateUrl: './passenger-detail.component.html',
-  styleUrls: ['./passenger-detail.component.css']
+  styleUrls: ['./passenger-detail.component.scss']
 })
 export class PassengerDetailComponent implements OnInit {
 
+  @Input()
+  detail : Passenger
   constructor() { }
 
   ngOnInit() {
